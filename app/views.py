a2b19a902761ba5885c31b4d4c5a9256a1496ff2
Nagaranjitha Adhikari <nagaranjithareddy421@gmail.com>
2024-04-13 16:55:59 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -29,3 +29,42 @@ def dropdowns(request):
 
 def forms(request):
     return render(request,'forms.html')
+
+def inputgroup(request):
+    return render(request,'inputgroup.html')
+
+def jumbotron(request):
+    return render(request,'jumbotron.html')
+
+def listgroup(request):
+    return render(request,'listgroup.html')
+
+def mediaobject(request):
+    return render(request,'mediaobject.html')
+
+def modal(request):
+    return render(request,'modal.html')
+
+def navs(request):
+    return render(request,'navs.html')
+def navbar(request):
+    return render(request,'navbar.html')
+def pagination(request):
+    return render(request,'pagination.html')
+def popovers(request):
+    return render(request,'popovers.html')
+
+def progress(request):
+    return render(request,'progress.html')
+
+def scrollspy(request):
+    return render(request,'scrollspy.html')
+
+def spinners(request):
+    return render(request,'spinners.html')
+
+def toasts(request):
+    return render(request,'toasts.html')
+
+def tooltips(request):
+    return render(request,'tooltips.html')
